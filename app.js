c56af232e59dce90606566c3aaa4d5a8840c2b51
roibh <roi@ewave.co.il>
2015-12-14 13:09:22 +0200

    nodulus

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ global.appRoot = __dirname;
 
 var fs = require('fs');
 var path = require('path');
-var favicon = require('serve-favicon');
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
