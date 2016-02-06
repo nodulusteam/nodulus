e364dcff7a6aad443de2056d8170e34ca6a902e4
roibh <roi@ewave.co.il>
2016-02-06 03:05:19 +0200

    typescript

--- classes/dal.js
+++ classes/dal.js
@@ -1,33 +1,22 @@
-﻿/*                 _       _           
-                 | |     | |          
-  _ __   ___   __| |_   _| |_   _ ___ 
+/*                 _       _
+                 | |     | |
+  _ __   ___   __| |_   _| |_   _ ___
  | '_ \ / _ \ / _` | | | | | | | / __|
  | | | | (_) | (_| | |_| | | |_| \__ \
  |_| |_|\___/ \__,_|\__,_|_|\__,_|___/
- @ewave open source | ©Roi ben haim  ®2016    
+ @ewave open source | ©Roi ben haim  ®2016
  */
-  
- 
-
-var Dal = (function () {
-    
-    
+/// <reference path="../typings/node/node.d.ts" />
+var dalModule = (function () {
     var util = require('util');
     var fs = require('fs');
     var path = require('path');
-    
     var impl = require('./impl/mongodb.js');
-    
     //fs.readFile('./config/site.json', 'utf8', function (err, data) {
     //    if (err) throw err;
     //    obj = JSON.parse(data);
     //});   
-    
     var private_variable = 'value';
-    function private_function() {
-        return obj;
-    }
-    
     function _query(queryStr, params, callback) {
         impl.query(queryStr, params, callback);
     }
@@ -37,43 +26,33 @@ var Dal = (function () {
     function _getAll(queryStr, params, callback) {
         impl.getAll(queryStr, params, callback);
     }
-    
     function _getCollection(name, callback) {
         impl.getCollection(name, callback);
     }
-    
     function _getSchema(name, callback) {
         impl.getSchema(name, callback);
     }
-    
     function _saveSchema(name, schema, callback) {
         impl.saveSchema(name, schema, callback);
     }
-    
-    
     function _deleteCollection(name, id, callback) {
         impl.deleteCollection(name, id, callback);
     }
     function _getSingle(name, id, callback) {
         impl.getSingle(name, id, callback);
     }
-    
     function _pushObject(id, collectionName, propertyName, pushObject, callback) {
         impl.pushObject(id, collectionName, propertyName, pushObject, callback);
     }
-    
     function _pullObject(id, collectionName, propertyName, pullObject, callback) {
         impl.pullObject(id, collectionName, propertyName, pullObject, callback);
     }
     function _getSet(idArr, collection, callback) {
         impl.getSet(idArr, collection, callback);
     }
-    
     function _addToSet(id, collectionName, propertyName, pushObject, callback) {
         impl.addToSet(id, collectionName, propertyName, pushObject, callback);
     }
-    
-    
     return {
         connect: _connect,
         query: _query,
@@ -90,4 +69,5 @@ var Dal = (function () {
     };
 })();
 // node.js module export
-module.exports = Dal;
+module.exports = dalModule;
+//# sourceMappingURL=dal.js.map
--- classes/impl/mongodb.ts
+++ classes/impl/mongodb.ts
@@ -1,4 +1,4 @@
-/*                 _       _
+﻿/*                 _       _
                  | |     | |
   _ __   ___   __| |_   _| |_   _ ___
  | '_ \ / _ \ / _` | | | | | | | / __|
@@ -7,13 +7,18 @@
  @ewave open source | ©Roi ben haim  ®2016
  */
 /// <reference path="../../typings/node/node.d.ts" />
+
 var Dal = (function () {
+
+
     var util = require('util');
     var fs = require('fs');
     var path = require('path');
     var config = require('../config.js');
     var ObjectID = require("mongodb").ObjectID;
+
     var assert = require('assert');
+
     function mongoOperator(key) {
         var ops = {
             "=": "$eq",
@@ -23,31 +28,56 @@ var Dal = (function () {
             "<": "$lt",
             "<=": "$lte",
             "in": "$in"
-        };
+
+        }
+
         if (ops[key] === undefined)
             return key;
+
         return ops[key];
+
+
     }
+
+
     function parse(str, params) {
-        var res = { queryMode: null, collection: "", where: {}, values: {}, limit: 0 };
-        var x = str.split(" ");
+        var res = { queryMode: null, collection: "", where: {}, values: {}, limit: 0 }
+
+        var x = str.split(" ")
         res.queryMode = x[0].trim();
+
         if (x[2] == "SET") {
-            res.values = { "$set": {} };
+            res.values = { "$set": {} }
             var pairs = str.substring(str.indexOf("SET") + 3).split(",");
             for (var j = 0; j < pairs.length; j++) {
                 var triple = pairs[j].split("=");
                 res.values["$set"][triple[0].trim()] = params[triple[0].trim()];
             }
         }
+
+
+
+
         for (var i = 0; i < x.length; i++) {
             if (x[i] == "UPDATE")
                 res.collection = x[i + 1];
+
+
+
+
+
+
             if (x[i] == "FROM")
                 res.collection = x[i + 1];
+
+
             if (x[i] == "INTO") {
+
+
+
                 res.collection = x[i + 1];
                 res.values = params;
+
             }
             if (x[i] == "WHERE") {
                 var conditionPoint = res.where;
@@ -55,35 +85,62 @@ var Dal = (function () {
                     conditionPoint["$query"] = {};
                     conditionPoint = conditionPoint["$query"];
                 }
+
+
                 var pairs = str.substring(str.indexOf("WHERE") + 6).split("AND");
                 for (var j = 0; j < pairs.length; j++) {
                     var triple = pairs[j].split("@");
                     if (triple.length < 2)
                         continue;
+
                     var cleankey = triple[1].replace(';', '').trim();
+
+
                     if (cleankey === "$limit")
                         res.limit = params[cleankey];
+
                     var operator = mongoOperator(triple[0].replace(cleankey, '').trim());
+
                     if (operator !== "^^") {
                         conditionPoint[cleankey] = {};
+
+
                         conditionPoint[cleankey][operator] = params[cleankey];
+
                         if (params[cleankey] == "false")
                             conditionPoint[cleankey][operator] = false;
                         if (params[cleankey] == "true")
                             conditionPoint[cleankey][operator] = true;
+
+
                     }
                     else {
                         res.where[cleankey] = params[cleankey];
                     }
+
+
+
+
                 }
+
+
             }
         }
+
+
         return res;
+
+
     }
+
+
+
+
     function _getAll(callback) {
         var url = config.appSettings().mongodb.host;
         var Db = require('mongodb').Db;
         var Server = require('mongodb').Server;
+
         var db = new Db('scripter', new Server('localhost', 27017));
         // Establish connection to db
         db.open(function (err, db1) {
@@ -99,90 +156,140 @@ var Dal = (function () {
             });
         });
     }
+
     function _getCollection(name, callback) {
         Dal.query("SELECT * FROM " + name, {}, callback);
     }
+
     function _getSingle(name, id, callback) {
+
         _connect(function (err, db) {
             assert.equal(null, err);
+
+
+
             db.collection(name).findOne({ "_id": ObjectID(id) }, function (err, doc) {
                 if (err !== null || doc === null)
                     callback({ "error": "not found" });
                 else
                     callback(doc);
             });
+
+
         });
+
     }
+
     this.db = null;
     function _connect(callback) {
         var url = config.appSettings().mongodb.host;
         var MongoClient = require('mongodb').MongoClient;
         if (Dal.db === null) {
             MongoClient.connect(url, function (err, db) {
+
                 Dal.db = db;
                 callback(err, db);
             });
-        }
-        else {
+        } else {
             callback(null, Dal.db);
         }
+
+
     }
+
     function _saveSchema(name, schema, callback) {
         Dal.query("INSERT INTO schemas name=@name, schema=@schema", { "name": name, "schema": schema }, callback);
     }
+
     function _getSchema(name, callback) {
         Dal.query("SELECT * FROM schemas WHERE name=@name", { "name": name }, callback);
     }
+
+
     //function _insertOrUpdate(queryStr, params, callback) {
+
     //    var oQuery = parse(queryStr, params);
     //    var url = config.appSettings().mongodb.host;
     //    var MongoClient = require('mongodb').MongoClient;
+
     //    collection.insert({ hello: 'world_no_safe' });
+
     //}
+
     function _deleteCollection(collection, id, callback) {
         var url = config.appSettings().mongodb.host;
         var MongoClient = require('mongodb').MongoClient;
         _connect(function (err, db) {
             assert.equal(null, err);
+
             db.collection(collection).findAndRemove({ "id": id }, function (err, doc) {
                 assert.equal(null, err);
+
                 callback(doc);
             });
         });
     }
+
+
+
     function _addToSet(id, collection, propertyName, pushObject, callback) {
         _connect(function (err, db) {
             assert.equal(null, err);
+
             var pusher = {};
             pusher[propertyName] = pushObject;
             db.collection(collection).update({ _id: id }, { $addToSet: pusher }, function (err, data) {
+
                 callback(data);
             });
+
+
+
         });
+
+
+
     }
+
+
     function _pushObject(id, collection, propertyName, pushObject, callback) {
         _connect(function (err, db) {
             assert.equal(null, err);
+
             var pusher = {};
             pusher[propertyName] = pushObject;
             db.collection(collection).update({ _id: id }, { $push: pusher }, function (err, data) {
+
                 callback(data);
             });
+
+
+
         });
+
+
+
     }
+
     function _pullObject(id, collection, propertyName, pullObject, callback) {
         _connect(function (err, db) {
             assert.equal(null, err);
             var puller = {};
+
             puller[propertyName] = pullObject;
+
             db.collection(collection).update({ _id: id }, { $pull: puller }, function (err, data) {
                 callback(data);
             });
         });
     }
+
     function _getSet(idArr, collection, callback) {
+
         if (typeof (idArr) == "string")
             idArr = [idArr];
+
+
         _connect(function (err, db) {
             assert.equal(null, err);
             db.collection(collection).find({ _id: { "$in": idArr } }).toArray(function (err, data) {
@@ -190,58 +297,83 @@ var Dal = (function () {
             });
         });
     }
+
+
+
+
     function _query(queryStr, params, callback) {
         var oQuery = parse(queryStr, params);
+        
+        
+        
+        
         //if (oQuery.where["$query"]["_id"] !== undefined) {
         //    oQuery.where["$query"]["_id"] = { ObjectID(oQuery.where["_id"]);
         //}
+        
+        
         _connect(function (err, db) {
             assert.equal(null, err);
+
             switch (oQuery.queryMode) {
                 case "INSERT":
+
                     if (oQuery.values["_id"] === undefined)
                         oQuery.values["_id"] = require("node-uuid").v4();
-                    db.collection(oQuery.collection).save(oQuery.values, function (err, result) {
-                        //if (result.result.upserted !== undefined || result.result.nModified == 1) {
-                        //    sendToArchive(oQuery, result);
-                        //}
-                        assert.equal(err, null);
-                        console.log("inserted document from " + oQuery.collection);
-                        callback(result);
-                    });
+
+                    db.collection(oQuery.collection).save(oQuery.values
+                        , function (err, result) {
+                            //if (result.result.upserted !== undefined || result.result.nModified == 1) {
+                            //    sendToArchive(oQuery, result);
+                            //}
+                        
+                        
+                            assert.equal(err, null);
+                            console.log("inserted document from " + oQuery.collection);
+                            callback(result);
+                        });
+
                     break;
                 case "DELETE":
-                    db.collection(oQuery.collection).remove(oQuery.where["$query"], function (err, result) {
-                        //if (result.result.ok == 1) {
-                        //    sendToArchive(oQuery, result);
-                        //}
-                        assert.equal(err, null);
-                        console.log("deleted document from " + oQuery.collection);
-                        callback(result);
-                    });
+                    db.collection(oQuery.collection).remove(oQuery.where["$query"]
+                        , function (err, result) {
+                            //if (result.result.ok == 1) {
+                            //    sendToArchive(oQuery, result);
+                            //}
+                            assert.equal(err, null);
+                            console.log("deleted document from " + oQuery.collection);
+                            callback(result);
+                        });
+
                     break;
-                    db.collection(oQuery.collection).remove(oQuery.where, function (err, result) {
-                        assert.equal(err, null);
-                        console.log("deleted document from " + oQuery.collection);
-                        callback(result);
-                    });
+
+                    db.collection(oQuery.collection).remove(oQuery.where
+                        , function (err, result) {
+                            assert.equal(err, null);
+                            console.log("deleted document from " + oQuery.collection);
+                            callback(result);
+                        });
+
                     break;
                 case "UPDATE":
-                    db.collection(oQuery.collection).update(oQuery.where, oQuery.values, function (err, result) {
-                        assert.equal(err, null);
-                        console.log("updated document from " + oQuery.collection);
-                        var cursor = db.collection(oQuery.collection).find(oQuery.where);
-                        var retArr = [];
-                        cursor.each(function (err, doc) {
+                    db.collection(oQuery.collection).update(oQuery.where, oQuery.values
+                        , function (err, result) {
+
                             assert.equal(err, null);
-                            if (doc != null) {
-                                retArr.push(doc);
-                            }
-                            else {
-                                callback(retArr);
-                            }
+                            console.log("updated document from " + oQuery.collection);
+                            var cursor = db.collection(oQuery.collection).find(oQuery.where);
+                            var retArr = [];
+                            cursor.each(function (err, doc) {
+                                assert.equal(err, null);
+                                if (doc != null) {
+                                    retArr.push(doc);
+
+                                } else {
+                                    callback(retArr)
+                                }
+                            });
                         });
-                    });
+
                     break;
                 case "SELECT":
                     var retArr = [];
@@ -249,30 +381,40 @@ var Dal = (function () {
                     var whereFlag = false;
                     for (var i in oQuery.where)
                         whereFlag = true;
+
                     if (whereFlag)
                         cursor = db.collection(oQuery.collection).find(oQuery.where);
                     else
                         cursor = db.collection(oQuery.collection).find();
+
                     if (oQuery.limit === undefined)
                         oQuery.limit = 0;
+
+
+
                     cursor.limit(oQuery.limit).each(function (err, doc) {
                         assert.equal(err, null);
                         if (doc != null) {
                             retArr.push(doc);
-                        }
-                        else {
-                            callback(retArr);
+
+                        } else {
+                            callback(retArr)
                         }
                     });
                     break;
             }
         });
+
     }
+
     function sendToArchive(data, res) {
+
         //if (res.result.upserted !== undefined)
         //    data.docIdentifier = res.result.upserted[0]._id;
         //socket.emit('dbchanges', data);
+
     }
+
     return {
         //insertOrUpdate: _insertOrUpdate,
         connect: _connect,
@@ -288,8 +430,8 @@ var Dal = (function () {
         pullObject: _pullObject,
         addToSet: _addToSet,
         db: null
+
     };
 })();
 // node.js module export
 module.exports = Dal;
-//# sourceMappingURL=mongodb.js.map
